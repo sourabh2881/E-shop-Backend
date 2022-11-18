0215573efcd33092f8ede36836417ba4a2c779cc
Sourabh Aggarwal <sourabha164@gmail.com>
2022-11-18 13:35:25 +0530

    update search for space seperated string

--- Case_Study/src/main/java/com/sourabh/Service/ProductService.java
+++ Case_Study/src/main/java/com/sourabh/Service/ProductService.java
@@ -1,6 +1,14 @@
 package com.sourabh.Service;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +27,9 @@ public class ProductService {
 	
 	@Autowired
 	private ModelMapper  modelMapper;
+
+	@Autowired
+	EntityManager em; 
 	
 	public Products addProduct(ProductsRequest req ){
 		Products prod = this.modelMapper.map(req, Products.class);
@@ -43,13 +54,41 @@ public class ProductService {
 	}
 
 	public List<Products> getByCategory(String category) {
+		
 		List<Products> prod = productRepo.findByCategory(category);
 		return prod;
 	}
 	
 	public List<Products> searchString(String searchString) {
-		List<Products> prod = productRepo.searchByNameLike(searchString);
-		return prod;
+		String trimmed = searchString.replaceAll("\\s{2,}", " ").trim();
+		String[] searchArray = trimmed.split(" ");
+		if(searchArray.length==0) {
+			return productRepo.findAll();
+		}
+	
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Products> cq = cb.createQuery(Products.class);
+		Root<Products> productRoot = cq.from(Products.class);
+		List<Predicate> predicates = new LinkedList<>();
+		
+		Predicate byName = cb.like(productRoot.get("name"), "%" + searchArray[0] + "%");
+		Predicate byCategory = cb.like(productRoot.get("category"), "%" + searchArray[0] + "%");
+		Predicate result = cb.or(byName,byCategory);
+
+		for(int i=1; i<searchArray.length;i++) {
+			System.out.println(searchArray[i]);
+			byCategory = cb.like(productRoot.get("category"), "%" + searchArray[i] + "%");
+			result = cb.or(result,byCategory);
+			byName = cb.like(productRoot.get("name"), "%" + searchArray[i] + "%");
+			result = cb.or(result,byName);
+		}
+		
+		predicates.add(result);
+		Predicate[] predArray = new Predicate[predicates.size()];
+		predicates.toArray(predArray);
+		cq.where(predArray);
+		TypedQuery<Products> query = em.createQuery(cq);
+		return query.getResultList();
 	}
 
 	public List<Products> filterPrice(FilterRequest req, String category) {
